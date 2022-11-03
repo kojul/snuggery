1916e4afb1872de7351aa60b5522f24b6effb181
kojul <kojul@kojul.com>
2022-11-03 06:05:32 -0400

    Remove ugly tabs, replace with spaces Removed fonts, added Iosevka

--- src/game/Boot.hx
+++ src/game/Boot.hx
@@ -1,74 +1,74 @@
 /**
-	Boot class is the entry point for the app.
-	It doesn't do much, except creating Main class and taking care of loops. Thus, you shouldn't be doing too much in this class.
+  Boot class is the entry point for the app.
+  It doesn't do much, except creating Main class and taking care of loops. Thus, you shouldn't be doing too much in this class.
 **/
 
 class Boot extends hxd.App {
-	#if debug
-	// Debug controls over game speed
-	var tmodSpeedMul = 1.0;
+  #if debug
+  // Debug controls over game speed
+  var tmodSpeedMul = 1.0;
 
-	// Shortcut to controller
-	var ca(get,never) : ControllerAccess<GameAction>;
-		inline function get_ca() return App.ME.ca;
-	#end
-
-
-	/**
-		App entry point: everything starts here
-	**/
-	static function main() {
-		new Boot();
-	}
-
-	/**
-		Called when engine is ready, actual app can start
-	**/
-	override function init() {
-		new App(s2d);
-		onResize();
-	}
-
-	// Window resized
-	override function onResize() {
-		super.onResize();
-		dn.Process.resizeAll();
-	}
+  // Shortcut to controller
+  var ca(get,never) : ControllerAccess<GameAction>;
+    inline function get_ca() return App.ME.ca;
+  #end
 
 
-	/** Main app loop **/
-	override function update(deltaTime:Float) {
-		super.update(deltaTime);
+  /**
+    App entry point: everything starts here
+  **/
+  static function main() {
+    new Boot();
+  }
 
-		// Debug controls over app speed
-		var adjustedTmod = hxd.Timer.tmod;
-		#if debug
-		if( App.exists() ) {
-			// Slow down (toggle)
-			if( ca.isPressed(DebugSlowMo)  )
-				tmodSpeedMul = tmodSpeedMul>=1 ? 0.2 : 1;
-			adjustedTmod *= tmodSpeedMul;
+  /**
+    Called when engine is ready, actual app can start
+  **/
+  override function init() {
+    new App(s2d);
+    onResize();
+  }
 
-			// Turbo (by holding a key)
-			adjustedTmod *= ca.isDown(DebugTurbo) ? 5 : 1;
-		}
-		#end
+  // Window resized
+  override function onResize() {
+    super.onResize();
+    dn.Process.resizeAll();
+  }
 
-		#if( hl && !debug )
-		try {
-		#end
 
-			// Run all dn.Process instances loops
-			dn.Process.updateAll(adjustedTmod);
+  /** Main app loop **/
+  override function update(deltaTime:Float) {
+    super.update(deltaTime);
 
-			// Update current sprite atlas "tmod" value (for animations)
-			Assets.update(adjustedTmod);
+    // Debug controls over app speed
+    var adjustedTmod = hxd.Timer.tmod;
+    #if debug
+    if( App.exists() ) {
+      // Slow down (toggle)
+      if( ca.isPressed(DebugSlowMo)  )
+        tmodSpeedMul = tmodSpeedMul>=1 ? 0.2 : 1;
+      adjustedTmod *= tmodSpeedMul;
 
-		#if( hl && !debug )
-		} catch(err) {
-			App.onCrash(err);
-		}
-		#end
-	}
+      // Turbo (by holding a key)
+      adjustedTmod *= ca.isDown(DebugTurbo) ? 5 : 1;
+    }
+    #end
+
+    #if( hl && !debug )
+    try {
+    #end
+
+      // Run all dn.Process instances loops
+      dn.Process.updateAll(adjustedTmod);
+
+      // Update current sprite atlas "tmod" value (for animations)
+      Assets.update(adjustedTmod);
+
+    #if( hl && !debug )
+    } catch(err) {
+      App.onCrash(err);
+    }
+    #end
+  }
 }
 
--- src/game/Types.hx
+++ src/game/Types.hx
@@ -1,48 +1,48 @@
-/**	This abstract enum is used by the Controller class to bind general game actions to actual keyboard keys or gamepad buttons. **/
+/**  This abstract enum is used by the Controller class to bind general game actions to actual keyboard keys or gamepad buttons. **/
 enum abstract GameAction(Int) to Int {
-	var MoveLeft;
-	var MoveRight;
-	var MoveUp;
-	var MoveDown;
+  var MoveLeft;
+  var MoveRight;
+  var MoveUp;
+  var MoveDown;
 
-	var Jump;
-	var Restart;
+  var Jump;
+  var Restart;
 
-	var MenuUp;
-	var MenuDown;
-	var MenuOk;
-	var MenuCancel;
-	var Pause;
+  var MenuUp;
+  var MenuDown;
+  var MenuOk;
+  var MenuCancel;
+  var Pause;
 
-	var ToggleDebugDrone;
-	var DebugDroneZoomIn;
-	var DebugDroneZoomOut;
-	var DebugTurbo;
-	var DebugSlowMo;
-	var ScreenshotMode;
+  var ToggleDebugDrone;
+  var DebugDroneZoomIn;
+  var DebugDroneZoomOut;
+  var DebugTurbo;
+  var DebugSlowMo;
+  var ScreenshotMode;
 }
 
 /** Entity state machine. Each entity can only have 1 active State at a time. **/
 enum abstract State(Int) {
-	var Normal;
+  var Normal;
 }
 
 
 /** Entity Affects have a limited duration in time and you can stack different affects. **/
 enum abstract Affect(Int) {
-	var Stun;
+  var Stun;
 }
 
 enum abstract LevelMark(Int) to Int {
-	var M_Coll_Wall; // 0
+  var M_Coll_Wall; // 0
 }
 
 enum abstract LevelSubMark(Int) to Int {
-	var SM_None; // 0
+  var SM_None; // 0
 }
 
 enum abstract SlowMoId(Int) to Int {
-	var S_Default; // 0
+  var S_Default; // 0
 }
 
 enum abstract ChargedAction(Int) to Int {
