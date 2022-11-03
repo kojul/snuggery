101c22502ed7be7c7cb92d2b9523bd3914cd8a89
kojul <kojul@kojul.com>
2022-11-03 06:17:20 -0400

    Missed some spaces, added Iosevka 12px

--- src/game/assets/Lang.hx
+++ src/game/assets/Lang.hx
@@ -20,8 +20,8 @@ class Lang {
                 hxd.Res.load("lang/"+CUR+".po");
             }
 
-		t = new GetText();
-		t.readPo( res.entry.getBytes() );
+    t = new GetText();
+    t.readPo( res.entry.getBytes() );
     }
 
     public static function untranslated(str:Dynamic) : LocaleString {
@@ -34,14 +34,14 @@ class Lang {
         Return a simple language code, depending on current System setting (eg. "en", "fr", "de" etc.). If something goes wrong, this returns "en".
         See: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
     **/
-	public static function getSystemLang() : String {
-		try {
-			var code = hxd.System.getLocale();
-			if( code.indexOf("-")>=0 )
-				code = code.substr(0,code.indexOf("-") );
-			return code.toLowerCase();
-		}
-		catch(_)
+  public static function getSystemLang() : String {
+    try {
+      var code = hxd.System.getLocale();
+      if( code.indexOf("-")>=0 )
+        code = code.substr(0,code.indexOf("-") );
+      return code.toLowerCase();
+    }
+    catch(_)
             return "en";
-	}
+  }
 }
