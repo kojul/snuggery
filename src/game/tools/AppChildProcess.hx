0c7fe748d634b694804efb56086a7df57446cce8
kojul <kojul@kojul.com>
2022-11-03 06:05:32 -0400

    Remove ugly tabs, replace with spaces

--- src/game/tools/AppChildProcess.hx
+++ src/game/tools/AppChildProcess.hx
@@ -1,17 +1,17 @@
 package tools;
 
 class AppChildProcess extends dn.Process {
-	public static var ALL : FixedArray<AppChildProcess> = new FixedArray(32);
+  public static var ALL : FixedArray<AppChildProcess> = new FixedArray(32);
 
-	public var app(get,never) : App; inline function get_app() return App.ME;
+  public var app(get,never) : App; inline function get_app() return App.ME;
 
-	public function new() {
-		super(App.ME);
-		ALL.push(this);
-	}
+  public function new() {
+    super(App.ME);
+    ALL.push(this);
+  }
 
-	override function onDispose() {
-		super.onDispose();
-		ALL.remove(this);
-	}
+  override function onDispose() {
+    super.onDispose();
+    ALL.remove(this);
+  }
 }
